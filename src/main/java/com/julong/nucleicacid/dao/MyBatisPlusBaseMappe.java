36377be45094f23dfc22a859b078c46372eabf6d
Eastward <960121759@qq.com>
2022-04-19 16:31:18 +0800

    挂号信息

---
+++ src/main/java/com/julong/nucleicacid/dao/MyBatisPlusBaseMappe.java
@@ -0,0 +1,8 @@
+package com.julong.nucleicacid.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MyBatisPlusBaseMappe<T> extends BaseMapper<T> {
+}
